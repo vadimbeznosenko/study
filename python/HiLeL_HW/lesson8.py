fa97dd13f418e62edbb47b81765cf773eb7e2aa5
Vadym Shelest <sax4@ukr.net>
2024-02-22 18:40:22 +0200

    add HW lesson8

---
+++ python/HiLeL_HW/lesson8.py
@@ -0,0 +1,44 @@
+
+# list_s = [0 for list_s in range(0, 100)]
+
+# for list_ in range(0, 100):
+#     list_s = list(list_ * "0")
+#     list_s.append("1")
+#     list_s.insert(0, "1")
+# print (list_s)
+    
+# list_s = [val for val in range(0, 45) if val % 2 == 0]
+# print(list_s)
+
+
+# list_s = [10, 4, 2, 6, 8, 1, 11, 12, 13]
+# number_ = int(input("enter number: "))
+# list_ss = [f"number {number_} in list" if number_ in list_s else f"number {number_} not in list " for val in list_s]
+# print(list_ss)
+
+# list_s = [10, 4, 2, 6, 8, 1, 11, 12, 13, 13]
+# list_max = max(list_s)
+# list_min = min(list_s)
+# print(list_max, list_min)
+
+# list_1 = [10, 4, 2, 6, 8, 1, 11, 12, 13, 13]
+# list_unic = [val for val in list_1 if list_1.count(val) > 1]
+# print(list_unic)
+
+
+# list_s = [10, 4, 2, 6, 8, 1, 11, 12, 13, 13]
+# print(list_s)
+# list_max = max(list_s)
+# list_min = min(list_s)
+# print(list_min, list_max)
+# index_max = list_s.index(list_max)
+# index_min = list_s.index(list_min)
+# list_s.remove(list_min)
+# list_s.remove(list_max)
+# list_s.insert(index_max, list_min)
+# list_s.insert(index_min, list_max)
+# print(list_s)
+
+list_1 = [10, 4, 2, 6, 8, 1, 11, 12, 13, 13]
+list_1.reverse()
+print(list_1)
